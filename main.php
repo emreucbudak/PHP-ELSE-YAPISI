75349c6f7f3d657b8746d3d1a7d20938e5dcfd57
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-25 21:46:44 +0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,20 @@
+<?php
+/******************************************************************************
+
+Emre Üçbudak Karadeniz Teknik Üniveristesi
+
+*******************************************************************************/
+// IF YAPISI
+$emre = 62;
+$uni = 60;
+if ($emre == 63 and $uni == 61) {
+    print("Urfaya  ve trabzona selamlar");
+}
+elseif ($emre == 63 and $uni != 61) {
+    print("Yanlızca Urfaya selamlar");
+}
+else {
+    print("HİÇBİR YERE SELAM YOK");
+}
+
+?>
